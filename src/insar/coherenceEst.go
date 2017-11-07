942481bc49fc8fd5c0489e098d22bb9fc30145ea
lightjameslyy <lightjameslyy@gmail.com>
2017-11-07 23:46:04 +0800

    finish coherenceEst

--- src/insar/coherenceEst.go
+++ src/insar/coherenceEst.go
@@ -1,79 +1,188 @@
 package insar
 
 import (
+	"bytes"
 	"fmt"
 	alluxio "github.com/Alluxio/alluxio-go"
 	"github.com/Alluxio/alluxio-go/option"
+	"io/ioutil"
 	"log"
-    "io/ioutil"
-    "time"
+	"math"
+	"math/cmplx"
+	"runtime"
+	"time"
+	"unsafe"
 )
 
+func getBuffer(client *alluxio.Client, path string) ([]byte, error) {
+	id, err := client.OpenFile(path, &option.OpenFile{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close(id)
+	readCloser, err := client.Read(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer readCloser.Close()
+	return ioutil.ReadAll(readCloser)
+}
+
+func CompareComplex64(ip string, path1 string, path2 string, counts int) {
+	fs := alluxio.NewClient(ip, 39999, time.Minute)
+	buffer1, err := getBuffer(fs, path1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data1 := *(*[]complex64)(unsafe.Pointer(&buffer1))
+	len1 := len(data1)
+	if counts > len1 {
+		log.Fatal("counts larger than file1 length!")
+	}
+
+	buffer2, err := getBuffer(fs, path2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data2 := *(*[]complex64)(unsafe.Pointer(&buffer2))
+	len2 := len(data2)
+	if counts > len2 {
+		log.Fatal("counts larger than file2 length!")
+	}
+	if len1 != len2 {
+		log.Fatal("file length different!")
+	}
+	fmt.Println(len1, len2)
+	for i := 0; i < len1; i++ {
+		/*
+			if i < 67108870 {
+				continue
+			}
+				if i < counts {
+					fmt.Println(data1[i], data2[i])
+				}
+		*/
+		if i%1000000 == 0 {
+			fmt.Println(i)
+		}
+		diff := data1[i] - data2[i]
+		if math.Abs(float64(real(diff))) > 10e-6 || math.Abs(float64(imag(diff))) > 10e-6 {
+			fmt.Println(i, data1[i], data2[i])
+		}
+	}
+}
+
 func CoherenceFun(ip string, inM string, inS string, inFlat string, outCoh string, rLooks uint32, cLooks uint32, iCols uint32, iRows uint32) {
+	// get alluxio client
 	fs := alluxio.NewClient(ip, 39999, time.Minute)
 
-	fInMId, err := fs.OpenFile(inM, &option.OpenFile{})
+	// get input buffers
+	MBytes, err := getBuffer(fs, inM)
 	if err != nil {
 		log.Fatal(err)
 	}
-    fInMReader, err := fs.Read(fInMId)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fInMBytes, err := ioutil.ReadAll(fInMReader)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fmt.Println(len(fInMBytes))
-	defer fs.Close(fInMId)
+	fmt.Println(len(MBytes))
 
-	fInSId, err := fs.OpenFile(inS, &option.OpenFile{})
+	SBytes, err := getBuffer(fs, inS)
 	if err != nil {
 		log.Fatal(err)
 	}
-    fInSReader, err := fs.Read(fInSId)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fInSBytes, err := ioutil.ReadAll(fInSReader)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fmt.Println(len(fInSBytes))
-	defer fs.Close(fInSId)
+	fmt.Println(len(SBytes))
 
-	fInFlatId, err := fs.OpenFile(inFlat, &option.OpenFile{})
+	FlatBytes, err := getBuffer(fs, inFlat)
 	if err != nil {
 		log.Fatal(err)
 	}
-    fInFlatReader, err := fs.Read(fInFlatId)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fInFlatBytes, err := ioutil.ReadAll(fInFlatReader)
-    if err != nil {
-		log.Fatal(err)
-    }
-    fmt.Println(len(fInFlatBytes))
-	defer fs.Close(fInFlatId)
+	fmt.Println(len(FlatBytes))
 
-	fOutCohId, err := fs.OpenFile(outCoh, &option.OpenFile{})
+	ok, err := fs.Exists(outCoh, &option.Exists{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer fs.Close(fOutCohId)
+	if true == ok {
+		err := fs.Delete(outCoh, &option.Delete{})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	outCohId, err := fs.CreateFile(outCoh, &option.CreateFile{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fs.Close(outCohId)
+	/*
+		var message = []byte("Greetings traveller!")
+		n, err := fs.Write(outCohId, bytes.NewBuffer(message))
+		if err != nil {
+			log.Fatal(err)
+		}
+		n, err = fs.Write(outCohId, bytes.NewBuffer(message))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(n)
+	*/
 
-	/*var newRow uint32 = iRows / rLooks*/
-	/*var newCol uint32 = iCols / cLooks*/
+	var newRow uint32 = iRows / rLooks
+	var newCol uint32 = iCols / cLooks
 
-	/*mData := make([]complex64, rLooks*iCols)*/
-	/*sData := make([]complex64, rLooks*iCols)*/
-	/*ftData := make([]float32, rLooks*iCols)*/
-	/*outData := make([]complex64, newCol)*/
+	mDataAll := *(*[]complex64)(unsafe.Pointer(&MBytes))
+	sDataAll := *(*[]complex64)(unsafe.Pointer(&SBytes))
+	ftDataAll := *(*[]float32)(unsafe.Pointer(&FlatBytes))
+	outDataAllBytes := make([]byte, 8*newRow*newCol)
+	outDataAll := *(*[]complex64)(unsafe.Pointer(&outDataAllBytes))
+	/*
+		for i := 0; i < 10; i++ {
+			fmt.Println(mData[i])
+		}
+	*/
 
-	/*var covar, ftTemp complex64*/
-	/*var mCov, sCov float32*/
-	//for i := 0; i < newRow; ++i {
-	//}
-	
+	//var covar, ftTemp complex64
+	//var mCov, sCov float32
+
+	runtime.GOMAXPROCS(64)
+	for i := uint32(0); i < newRow; i++ {
+		//mData := mDataAll[i*rLooks*iCols:]
+		//sData := sDataAll[i*rLooks*iCols:]
+		//ftData := ftDataAll[i*rLooks*iCols:]
+		//outData := outDataAll[i*newCol:]
+
+		go func(mData []complex64, sData []complex64, ftData []float32, outData []complex64, newCol uint32) {
+			//for j := uint32(0); j < newCol; j++ {
+			for j := uint32(0); j < newCol; j++ {
+				var covar complex64 = complex(0, 0)
+				var mCov, sCov float32 = 0.0, 0.0
+				for ii := uint32(0); ii < rLooks; ii++ {
+					for jj := uint32(0); jj < cLooks; jj++ {
+						cur := ii*iCols + (j*cLooks + jj)
+						//fmt.Println(mData[cur], sData[cur], ftData[cur])
+						covar += complex64(complex128(mData[cur]) * cmplx.Conj(complex128(sData[cur])) * complex(math.Cos(float64(ftData[cur])), -math.Sin(float64(ftData[cur]))))
+						//mCov += float32(cmplx.Abs(complex128(mData[cur])))
+						//sCov += float32(cmplx.Abs(complex128(sData[cur])))
+						mCov += float32(real(mData[cur])*real(mData[cur]) + imag(mData[cur])*imag(mData[cur]))
+						sCov += float32(real(sData[cur])*real(sData[cur]) + imag(sData[cur])*imag(sData[cur]))
+						//fmt.Println(covar, mData[cur], mCov, sData[cur], sCov)
+					}
+				}
+				mCov = float32(math.Sqrt(float64(mCov)))
+				sCov = float32(math.Sqrt(float64(sCov)))
+				//fmt.Printf("%v %v %v\n", covar, mCov, sCov)
+				msCov := mCov * sCov
+				if math.Abs(float64(msCov)) > 10e-6 {
+					outData[j] = complex(real(covar)/msCov, imag(covar)/msCov)
+				} else {
+					outData[j] = complex(0, 0)
+				}
+				//fmt.Println(outData[j])
+			}
+		}(mDataAll[i*rLooks*iCols:], sDataAll[i*rLooks*iCols:], ftDataAll[i*rLooks*iCols:], outDataAll[i*newCol:], newCol)
+	}
+
+	fmt.Println(len(outDataAll))
+	//n, err := fs.Write(outCohId, bytes.NewReader(*(*[]byte)(unsafe.Pointer(&outDataAll))))
+	n, err := fs.Write(outCohId, bytes.NewReader(outDataAllBytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(n)
 }
--- src/main.go
+++ src/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	/*"fmt"*/
-	/*"log"*/
-	/*"time"*/
-
-    /*alluxio "github.com/Alluxio/alluxio-go"*/
-    /*"github.com/Alluxio/alluxio-go/option"*/
-    "insar"
+	"fmt"
+	"math/cmplx"
+	/*"log"*/ /*"time"*/ /*alluxio "github.com/Alluxio/alluxio-go"*/ /*"github.com/Alluxio/alluxio-go/option"*/
+	"insar"
 )
 
-func main() {
-    /*
-	fs := alluxio.NewClient("10.2.152.24", 39999, time.Second*5)
-	ok, err := fs.Exists("/insar/fineReg/1.rmg", &option.Exists{})
-	if err != nil {
-		log.Fatal(err)
+func TestComplex() {
+	var c complex64 = 3.0 + 4.0i
+	fmt.Printf("real: %f, imag: %f, value: %v, norm: %f\n", real(c), imag(c), c, cmplx.Abs(complex128(c)))
+}
 
-	}
-    */
+func main() {
+	/*
+		fs := alluxio.NewClient("10.2.152.24", 39999, time.Second*5)
+		ok, err := fs.Exists("/insar/fineReg/1.rmg", &option.Exists{})
+		if err != nil {
+			log.Fatal(err)
+
+		}
+	*/
 	//id, err := fs.CreateFile("/insar/2.txt", &option.CreateFile{})
 	//if err != nil {
 	//log.Fatal(err)
@@ -30,9 +32,20 @@ func main() {
 	//defer fs.Close(id)
 	//fmt.Println(n)
 
-	//fmt.Println(ok) 
+	//fmt.Println(ok)
 
-    insar.CoherenceFun("10.2.152.24", "/insar/fineReg/1.rmg", "/insar/fineReg/1.rmg", "/insar/flatPha/1-2.rmg", 
-                        "/insar/coh/1-2.rmg", 4, 2, 18707, 21180) 
+	insar.CoherenceFun("10.2.152.24", "/insar/fineReg/1.rmg", "/insar/fineReg/2.rmg", "/insar/flatPha/1-2.rmg",
+		"/insar/coh/1-2.rmg", 4, 2, 18707, 21180)
+	insar.CoherenceFun("10.2.152.24", "/insar/fineReg/1.rmg", "/insar/fineReg/4.rmg", "/insar/flatPha/1-4.rmg",
+		"/insar/coh/1-4.rmg", 4, 2, 18707, 21180)
+	insar.CoherenceFun("10.2.152.24", "/insar/fineReg/2.rmg", "/insar/fineReg/3.rmg", "/insar/flatPha/2-3.rmg",
+		"/insar/coh/2-3.rmg", 4, 2, 18707, 21180)
+	insar.CoherenceFun("10.2.152.24", "/insar/fineReg/2.rmg", "/insar/fineReg/4.rmg", "/insar/flatPha/2-4.rmg",
+		"/insar/coh/2-4.rmg", 4, 2, 18707, 21180)
+	insar.CoherenceFun("10.2.152.24", "/insar/fineReg/3.rmg", "/insar/fineReg/4.rmg", "/insar/flatPha/3-4.rmg",
+		"/insar/coh/3-4.rmg", 4, 2, 18707, 21180)
+	//insar.CompareComplex64("10.2.152.24", "/insar/coh/1-2.rmg", "/insar/linux_coh/1-2.rmg", 100)
+
+	//TestComplex()
 
 }
